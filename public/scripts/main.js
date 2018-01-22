9e9f2cd0c448c83598824d0a8643afc4db16bc26
Zoe Ingram <zoe.a.ingram@gmail.com>
2018-01-21 22:30:55 -0800

    Adds p5 sidebar

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,13 +1,26 @@
+var canvas;
+
+function setup() {
+  canvas = createCanvas(windowWidth/7, windowHeight);
+  canvas.parent('sidebar');
+
+}
+
+function draw() {
+  strokeWeight(2);
+  rect(2, 2, canvas.width -5, canvas.height -5);
+
+
+}
 $(document).ready(function() {
   var boxes = document.getElementsByClassName('box');
   var currentNode;
+
 
   $(document).click(function(event) {
       currentNode = $(event.target);
       console.log(currentNode.css('background-image'));
   });
-
-
 
 
 }); // End document ready

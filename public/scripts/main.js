d5fec13f54f467dc2ae653ea78787249f495860b
Zoe Ingram <zoe.a.ingram@gmail.com>
2018-01-23 14:18:17 -0800

    Adds parent image function

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -13,7 +13,10 @@ function draw() {
   rect(2, 30, canWidth -5, canvas.height -70);
 }
 
-function determineLane(nodeValue) {
+function determineLane(node) {
+  findImageByClick(node);
+  
+  var nodeValue =  node.context.innerHTML;
   if (nodeValue === '1') {
     console.log("hits 1");
   } else if (nodeValue === '2') {
@@ -25,6 +28,11 @@ function determineLane(nodeValue) {
   }
 }
 
+function findImageByClick(node) {
+  var imageURL = node.parent().css('background-image');
+  console.log(imageURL, "url");
+}
+
 
 
 $(document).ready(function() {
@@ -32,9 +40,8 @@ $(document).ready(function() {
   var currentValue;
 
   $(document).click(function(event) {
-      currentNode = $(event.target.innerHTML);
-      currentValue = currentNode.selector;
-      determineLane(currentValue);
+      currentNode = $(event.target);
+      determineLane(currentNode);
   });
 
 

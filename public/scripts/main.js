4c53924a69e311596f0c47efb2d9cb40742381a8
Zoe Ingram <zoe.a.ingram@gmail.com>
2018-01-23 13:45:18 -0800

    adds event listener for three option click

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -4,25 +4,37 @@ var canWidth;
 function setup() {
   canvas = createCanvas(windowWidth/7, windowHeight);
   canvas.parent('sidebar');
-
+  canvas.position(0, 0);
 }
 
 function draw() {
   strokeWeight(2);
   canWidth = canvas.width;
-
-
   rect(2, 30, canWidth -5, canvas.height -70);
 }
 
-$(document).ready(function() {
-  var boxes = document.getElementsByClassName('box');
-  var currentNode;
+function determineLane(nodeValue) {
+  if (nodeValue === '1') {
+    console.log("hits 1");
+  } else if (nodeValue === '2') {
+    console.log("hits 2");
+  } else if (nodeValue === '3'){
+    console.log("hits 3");
+  } else {
+    console.log("did not hit value");
+  }
+}
 
+
+
+$(document).ready(function() {
+  var currentNode;
+  var currentValue;
 
   $(document).click(function(event) {
-      currentNode = $(event.target);
-      console.log(currentNode.css('background-image'));
+      currentNode = $(event.target.innerHTML);
+      currentValue = currentNode.selector;
+      determineLane(currentValue);
   });
 
 
